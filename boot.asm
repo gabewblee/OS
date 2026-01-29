4dea4617c7cd649a074ac5ca7db44631acaf4489
gabewblee <gabe.wblee@gmail.com>
2026-01-29 16:01:18 -0500

    First-stage bootloader implemented

--- boot.asm
+++ boot.asm
@@ -1,26 +1,52 @@
-bits 16
 org 0x7C00
+bits 16
 
 start:
     cli
+    ; Initialize the ds, es, and ss registers to 0
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
+
+    ; Define drive variable
+    mov [drive], dl
+
+    ; Initialize the sp register to boot sector
     mov sp, 0x7C00
     sti
 
-    call print
-    jmp $
+    ; Rewrite boot sector to [0x0060:0x0000]
+    cld
+    mov si, 0x7C00
+    mov ax, 0x0060
+    mov es, ax
+    xor di, di
+    mov cx, 512
+    rep movsb
+    jmp 0x0060:relocated
 
-print:
-    mov al, 'H'
-    mov ah, 0x0E
-    int 0x10
-    mov al, 'i'
-    mov ah, 0x0E
-    int 0x10
-    ret
+relocated:
+    ; Reset ds register to 0x0060 so drive label works
+    mov ax, cs
+    mov ds, ax
+
+    ; Read partition boot sector
+    mov ah, 0x02
+    mov al, 0x01
+    mov ch, 0x00
+    mov cl, 0x02
+    mov dh, 0x00
+    mov dl, [drive]
+    xor ax, ax
+    mov es, ax
+    mov bx, 0x7C00
+    int 0x13
+
+    jmp 0x0000:0x7C00
+
+drive:
+    db 0
 
 times 510-($-$$) db 0
 dw 0xAA55
